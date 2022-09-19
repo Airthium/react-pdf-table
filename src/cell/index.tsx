6f6e31bae83ec01169186c22753371205faf7f83
Simon Garnotel <simon.garnotel@gmail.com>
2022-09-19 11:15:08 +0200

    remove node_modules

--- src/cell/index.tsx
+++ src/cell/index.tsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import { StyleSheet, Text } from '@react-pdf/renderer'
 
-const Cell = ({ cellData }) => {
+const Cell = ({ content }) => {
   const styles = StyleSheet.create({
     cell: {
       border: '1px solid black'
